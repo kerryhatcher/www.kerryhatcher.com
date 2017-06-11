39e3f6567d6d1e19de15d1b46a4c9f7038cfce03
Kerry Hatcher <kwhatcher@gmail.com>
2017-06-10 21:43:03 -0400

    Lets get circle going!

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -17,6 +17,12 @@ const Header = (props, { metadata: { pkg } }) => (
         >
           { "Home" }
         </Link>
+        <Link
+            className={ styles.link }
+            to={ "/projects" }
+        >
+            { "Projects" }
+        </Link>
       </div>
       <div className={ styles.navPart2 }>
         {
