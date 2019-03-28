17bf5d3d761ba843b60681546fe65ba62fdb580a
Kerry Hatcher <kerry.hatcher@coxautoinc.com>
2019-03-28 12:06:00 -0400

    Move to hugo

--- content/assets/gadstuff.js
+++
@@ -1,6 +0,0 @@
-
-(adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-6833587723662431",
-    enable_page_level_ads: true
-});
-cosole.log(window.adsbygoogle);
---
+++ content/posts/my-first-post.md
@@ -0,0 +1,6 @@
+---
+title: "My First Post"
+date: 2019-03-28T11:48:32-04:00
+draft: true
+---
+
